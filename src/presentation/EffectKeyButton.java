bbbbacb28b7338b8e8588ff497f0a7755d577384
nadineGoeb <n.goebertshan@web.de>
2015-01-28 18:43:53 +0100

    lightning effectkeybuttons
    Die EffectKEyButtons leuchten jetzt auf, wenn sie aktiviert sind :)

--- src/presentation/EffectKeyButton.java
+++ src/presentation/EffectKeyButton.java
@@ -1,24 +1,31 @@
 package presentation;
 
+import java.awt.Color;
 
 import repository.EffectMode;
 import services.AudioCore;
 import services.EffectModeBehavior;
 
-public class EffectKeyButton extends KeyButton{
+public class EffectKeyButton extends KeyButton {
 
 	private static final long serialVersionUID = 1L;
 	private AudioCore audioCore;
 	private final EffectMode MODE;
 	private EffectModeBehavior effectModeBehavior;
-	
-	public EffectKeyButton(String keyLabel, EffectMode mode, int keyCode, AudioCore audioCore) {
+	private Color colorStandart = new Color(85, 120, 10);
+	private Color colorActive = new Color(170, 240, 20);
+	boolean active = false;
+
+	public EffectKeyButton(String keyLabel, EffectMode mode, int keyCode,
+			AudioCore audioCore) {
 		super(keyLabel, keyCode);
 		this.audioCore = audioCore;
 		this.MODE = mode;
 		this.effectModeBehavior = EffectModeBehavior.getInstance();
-		
-		switch(mode){
+
+		setBackground(colorStandart);
+
+		switch (mode) {
 		case HIGHPASS:
 			setBigIcon(UserInterface.highpassIcon);
 			setToolTipText("Highpass Filter");
@@ -34,18 +41,24 @@ public class EffectKeyButton extends KeyButton{
 		default:
 			break;
 		}
-		
+
 		setOnTrigger(new Runnable() {
-			
+
 			@Override
 			public void run() {
 				effectModeBehavior.trigger(MODE);
+
+				if (active == false) {
+					active = true;
+					setBackground(colorActive);
+				} else {
+					active = false;
+					setBackground(colorStandart);
+				}
+
 			}
 		});
-		
-		
-		
+
 	}
-	
 
 }
